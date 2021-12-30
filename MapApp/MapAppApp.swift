b0cdd3b72b428643ae23f9a9a6fb54fb76188434
ybnr1130 <muuckk@gmail.com>
2021-12-31 06:39:40 +0900

    First commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by 薮内紀仁 on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
